ebb7d74eda0b36089245ad82af3b43491beea288
Henrique Linhares <linhares.luiz8@gmail.com>
2023-04-04 14:36:52 -0300

    Estudando operadores

--- variaveis/src/Operadores.java
+++ variaveis/src/Operadores.java
@@ -2,14 +2,12 @@ public class Operadores {
     public static void main(String[] args) {
         String concatenacao = "?";
 
-        concatenacao = 1+1+1+"1";
+        concatenacao = 1+1+1+"1"; /*O resultado será 31, pois a partir do momento que ele percerbe que tem um texto ele começa a concatenar em vez de somar. */
+
+        System.out.println(concatenacao); 
+
+        concatenacao = 1+"1"+1+1; /*O resultado aqui será 1111, pois quando ele percebe que tem um texto e faz a concatenação, todos os outros em diante também são lidos como texto. */
 
         System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+1; 
-
-        System.out.println(concatenacao);
-
-
     }
 }
