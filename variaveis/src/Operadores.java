fc8a543597e15a8174c57cc81f560004676570fb
Henrique Linhares <linhares.luiz8@gmail.com>
2023-04-04 11:36:10 -0300

    operadores-java

---
+++ variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1; 
+
+        System.out.println(concatenacao);
+
+
+    }
+}
