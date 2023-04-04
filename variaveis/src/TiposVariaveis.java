13323f2619a590c32e792d6f2df6d653d08c12dc
Henrique Linhares <linhares.luiz8@gmail.com>
2023-04-04 11:11:24 -0300

    Estudo de variaveis

---
+++ variaveis/src/TiposVariaveis.java
@@ -0,0 +1,12 @@
+
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        int primeiroNumero = 4;
+        int segundoNumero = 4;
+        int resultado = primeiroNumero + segundoNumero;
+        String nome = "Luiz Henrique";
+
+        System.out.println("O aluno se chama: " + nome + ". E a sua idade é: " + resultado + " anos.");
+
+    }
+}
