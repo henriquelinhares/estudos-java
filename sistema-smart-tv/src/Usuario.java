f48b60145e28641b7606308f066d95fa1915033d
Henrique Linhares <linhares.luiz8@gmail.com>
2023-04-05 11:53:57 -0300

    Projeto SmartTv

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -12,5 +12,18 @@ public class Usuario {
         smartTv.desligar ();
         System.out.println("Novo status -> TV Ligada? " + smartTv.ligada);
 
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+        smartTv.aumentarCanal();
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
     }
 }
